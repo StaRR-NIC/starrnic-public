314d64d39b147a8ea80f226c9e6c8f297ce1b861
Anup Agarwal <anupa@andrew.cmu.edu>
2021-12-02 18:30:35 +0000

    Update stream size in switches (it is in bytes not bits)

--- plugin/stream-switch/stream_switch_axis_switch_combiner_tdest.tcl
+++ plugin/stream-switch/stream_switch_axis_switch_combiner_tdest.tcl
@@ -1,7 +1,7 @@
 set axis_switch axis_switch_combiner_tdest
 create_ip -name axis_switch -vendor xilinx.com -library ip -module_name $axis_switch
 set_property -dict [list
-    CONFIG.TDATA_NUM_BYTES {512}
+    CONFIG.TDATA_NUM_BYTES {64}
     CONFIG.HAS_TKEEP {1}
     CONFIG.HAS_TLAST {1}
     CONFIG.TUSER_WIDTH {48}
