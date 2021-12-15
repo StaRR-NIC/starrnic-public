16637240e84044eda82f7144b0bf26ae259c2a01
Anup Agarwal <anupa@andrew.cmu.edu>
2021-12-15 19:00:19 +0000

    Scaffold

---
+++ sim/stream_switch/test_stream_switch.py
@@ -0,0 +1,84 @@
+import itertools
+import logging
+
+import cocotb
+from cocotb.clock import Clock
+from cocotb.regression import TestFactory
+from cocotb.triggers import RisingEdge
+from cocotbext.axi import (AxiLiteBus, AxiLiteMaster, AxiStreamBus,
+                           AxiStreamFrame, AxiStreamSink, AxiStreamSource)
+
+
+class TB:
+    def __init__(self, dut):
+        self.dut = dut
+
+        self.log = logging.getLogger("cocotb.tb")
+        self.log.setLevel(logging.DEBUG)
+
+        cocotb.fork(Clock(dut.axis_aclk, 2, units="ns").start())
+        cocotb.fork(Clock(dut.axil_aclk, 4, units="ns").start())
+
+        self.source_tx = AxiStreamSource(AxiStreamBus.from_prefix(dut, "s_axis_qdma_h2c"), dut.axis_aclk, dut.axis_aresetn, reset_active_level=False)
+        self.sink_tx = AxiStreamSink(AxiStreamBus.from_prefix(dut, "m_axis_qdma_c2h"), dut.axis_aclk, dut.axis_aresetn, reset_active_level=False)
+        self.source_rx = AxiStreamSource(AxiStreamBus.from_prefix(dut, "s_axis_adap_rx_250mhz"), dut.axis_aclk, dut.axis_aresetn, reset_active_level=False)
+        self.sink_tx = AxiStreamSink(AxiStreamBus.from_prefix(dut, "m_axis_adap_tx_250mhz"), dut.axis_aclk, dut.axis_aresetn, reset_active_level=False)
+        self.control = AxiLiteMaster(AxiLiteBus.from_prefix(dut, "s_axil"), dut.axil_aclk, dut.axil_aresetn)
+
+    def set_idle_generator(self, generator=None):
+        if generator:
+            self.source.set_pause_generator(generator())
+
+    def set_backpressure_generator(self, generator=None):
+        if generator:
+            self.sink.set_pause_generator(generator())
+
+    async def reset(self):
+        self.dut.mod_rstn.setimmediatevalue(1)
+        # mod rst signals are synced with the axilite clock not axistream clock
+        await RisingEdge(self.dut.axil_aclk)
+        await RisingEdge(self.dut.axil_aclk)
+        self.dut.mod_rstn.value = 0
+        await RisingEdge(self.dut.axil_aclk)
+        await RisingEdge(self.dut.axil_aclk)
+        self.dut.mod_rstn.value = 1
+        await RisingEdge(self.dut.mod_rst_done)
+
+async def run_test(dut, idle_inserter=None, backpressure_inserter=None):
+
+    tb = TB(dut)
+
+    await tb.reset()
+
+    tb.set_idle_generator(idle_inserter)
+    tb.set_backpressure_generator(backpressure_inserter)
+
+    test_frames = []
+
+    test_frame = AxiStreamFrame(b'101010101')
+    await tb.source_tx.send(test_frame)
+    test_frames.append(test_frame)
+
+    tb.log.info("Frames sent")
+
+    for test_frame in test_frames:
+        tb.log.info("Trying to recv frames")
+        rx_frame = await tb.sink_tx.recv()
+
+        assert rx_frame.tdata == test_frame.tdata
+
+    assert tb.sink.empty()
+
+    await RisingEdge(dut.axis_aclk)
+    await RisingEdge(dut.axis_aclk)
+
+
+def cycle_pause():
+    return itertools.cycle([1, 1, 1, 0])
+
+if cocotb.SIM_NAME:
+
+    factory = TestFactory(run_test)
+    factory.add_option("idle_inserter", [None, cycle_pause])
+    factory.add_option("backpressure_inserter", [None, cycle_pause])
+    factory.generate_tests()
