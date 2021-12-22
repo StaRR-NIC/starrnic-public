826202ad96beac3c90b9a2aeb9e06f598309812c
Anup Agarwal <anupa@andrew.cmu.edu>
2021-12-22 20:45:49 +0000

    Add non bypass config to configure script. Stream switch works in hw now

--- script/configure_stream_switch.sh
+++ script/configure_stream_switch.sh
@@ -9,8 +9,17 @@ sudo insmod onic.ko
 sudo ifconfig enp59s0 10.0.0.57/24 up
 
 cd $PCIMEM_PATH
+# Read temperature
 sudo ./pcimem /sys/bus/pci/devices/0000:3b:00.0/resource2 0x10400
-# sudo ./pcimem /sys/bus/pci/devices/0000:3b:00.0/resource2 0x100000
+
+# Read current splitter control register
+sudo ./pcimem /sys/bus/pci/devices/0000:3b:00.0/resource2 0x100000
+
+# Use counter
+# sudo ./pcimem /sys/bus/pci/devices/0000:3b:00.0/resource2 0x100044 w 0x00
+# sudo ./pcimem /sys/bus/pci/devices/0000:3b:00.0/resource2 0x100000 w 0x02
+
+# Bypass
 sudo ./pcimem /sys/bus/pci/devices/0000:3b:00.0/resource2 0x100040 w 0x00
 sudo ./pcimem /sys/bus/pci/devices/0000:3b:00.0/resource2 0x100000 w 0x02
 
