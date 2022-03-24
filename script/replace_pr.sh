be299c7e16be40b7870ebcbdc52017609b6566f2
Daehyeok Kim <dkimcs@gmail.com>
2022-03-24 17:45:45 +0000

    Update all scripts for U280 - scaffold also replace_pr.sh

--- script/replace_pr.sh
+++ script/replace_pr.sh
@@ -2,11 +2,11 @@
 
 # The addresses in this script and logic is specific to the stream-switch design
 
-if [ $# -eq 0 ] || [[ -z $XILINX_VIVADO ]] || [[ -z $PCIMEM ]] || [[ -z $EXTENDED_DEVICE_BDF ]] || [[ -z $STARRNIC_SHELL ]]; then
+if [ $# -eq 0 ] || [[ -z $XILINX_VIVADO ]] || [[ -z $PCIMEM ]] || [[ -z $EXTENDED_DEVICE_BDF1 ]] || [[ -z $STARRNIC_SHELL ]]; then
     echo "Usage: replace_pr.sh PARTIAL_BITSTREAM"
     echo "Please export PCIMEM to point to pcimem binary."
     echo "Please export STARRNIC_SHELL as path to starrnic_shell."
-    echo "Please export EXTENDED_DEVICE_BDF to point to the FPGA NIC."
+    echo "Please export EXTENDED_DEVICE_BDF1 to point to the FPGA NIC."
     echo "Please ensure vivado is in path."
     exit 1
 fi
@@ -18,28 +18,28 @@ partial_bitstream=$1
 
 bypass_region () {
     echo "Bypassing..."
-    sudo $PCIMEM /sys/bus/pci/devices/$EXTENDED_DEVICE_BDF/resource2 0x100040 w 0x00
-    sudo $PCIMEM /sys/bus/pci/devices/$EXTENDED_DEVICE_BDF/resource2 0x100044 w 0x80000000
-    sudo $PCIMEM /sys/bus/pci/devices/$EXTENDED_DEVICE_BDF/resource2 0x100000 w 0x02
+    sudo $PCIMEM /sys/bus/pci/devices/$EXTENDED_DEVICE_BDF1/resource2 0x100040 w 0x00
+    sudo $PCIMEM /sys/bus/pci/devices/$EXTENDED_DEVICE_BDF1/resource2 0x100044 w 0x80000000
+    sudo $PCIMEM /sys/bus/pci/devices/$EXTENDED_DEVICE_BDF1/resource2 0x100000 w 0x02
     echo ""
 }
 
 connect_region () {
     echo "Connecting..."
-    sudo $PCIMEM /sys/bus/pci/devices/$EXTENDED_DEVICE_BDF/resource2 0x100040 w 0x80000000
-    sudo $PCIMEM /sys/bus/pci/devices/$EXTENDED_DEVICE_BDF/resource2 0x100044 w 0x00
-    sudo $PCIMEM /sys/bus/pci/devices/$EXTENDED_DEVICE_BDF/resource2 0x100000 w 0x02
+    sudo $PCIMEM /sys/bus/pci/devices/$EXTENDED_DEVICE_BDF1/resource2 0x100040 w 0x80000000
+    sudo $PCIMEM /sys/bus/pci/devices/$EXTENDED_DEVICE_BDF1/resource2 0x100044 w 0x00
+    sudo $PCIMEM /sys/bus/pci/devices/$EXTENDED_DEVICE_BDF1/resource2 0x100000 w 0x02
     echo ""
 }
 
 read_counter_value () {
     echo "Reading..."
-    sudo $PCIMEM /sys/bus/pci/devices/$EXTENDED_DEVICE_BDF/resource2 0x140000
+    sudo $PCIMEM /sys/bus/pci/devices/$EXTENDED_DEVICE_BDF1/resource2 0x140000
 }
 
 check_read_temperature () {
     echo "Checking temperature..."
-    sudo $PCIMEM /sys/bus/pci/devices/$EXTENDED_DEVICE_BDF/resource2 0x10400
+    sudo $PCIMEM /sys/bus/pci/devices/$EXTENDED_DEVICE_BDF1/resource2 0x10400
     # TODO: check and assert if above output seems fine
 }
 
